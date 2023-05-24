255c2ae0ae2dc1b7d366243dee62e046ab1e0521
Yukaryavka <yukaryavka@gmail.com>
2023-05-24 16:37:58 +0900

    [update]デバッグ途中の旧データをコミットしてしまっていた為、ローカル最新版のデータを適応
    Signed-off-by: Yukaryavka <yukaryavka@gmail.com>

--- alpaca-lora/merge_gptneox_lora.py
+++ alpaca-lora/merge_gptneox_lora.py
@@ -1,17 +1,23 @@
-from dataclasses import dataclass, field
-from typing import Optional
-
-import peft
+import os
+import sys
 import torch
+import peft
+
+from typing import Optional
+from dataclasses import dataclass, field
 from peft import PeftConfig, PeftModel
-from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
 from peft.utils import _get_submodules
+from transformers import AutoModelForCausalLM, AutoTokenizer, HfArgumentParser
 
 if len(sys.argv) < 3:
+    print("\n")
+    print("<< merge_gpt_neox_lora.py - Reference >>")
+    print("")
     print("Usage: python merge_gptneox_lora.py base_model_name lora_model_name output_dir")
-    print("  base_model_name: Define the base model name or path to merge LoRA from / Example: 'rinna/japanese-gpt-neox-3.6b-instruction-sft'")
-    print("  lora_model_name: Define the model name or path of the LoRA to be merged into the base model. [Basically, set the path to the directory where adapter_model.bin and adapter_config.json are stored.]")
-    print("  output_dir: Define a directory to store the pytorch model in which the base and LoRA models are merged")
+    print("-------------------------------------------------------------------------------")
+    print("  base_model_name:  Define the base model name or dirpath or huggingFace repo to merge LoRA from / Example: 'rinna/japanese-gpt-neox-3.6b-instruction-sft'")
+    print("  lora_model_name:  Define the dirpath of the LoRA model to be merged into the base model. [Basically, set the path to the directory where adapter_model.bin and adapter_config.json are stored.]")
+    print("  output_dir:       Define a directory to store the pytorch model in which the base and LoRA models are merged")
     sys.exit(1)
 
 base_model_name = sys.argv[1]
@@ -21,13 +27,12 @@ output_dir = sys.argv[3]
 # make sure the output directory exists
 os.makedirs(output_dir, exist_ok=True)
 
-# Load base model and LoRA model
+# Load base model and tokenizer
 print(">>> Loading base model: ", base_model_name)
 model = AutoModelForCausalLM.from_pretrained(base_model_name, return_dict=True, torch_dtype=torch.float16)
-print(">>> Loading LoRA model: ", lora_model_name)
-tokenizer = AutoTokenizer.from_pretrained(lora_model_name)
+tokenizer = AutoTokenizer.from_pretrained(base_model_name)
 
-print(">>> Preparing for merge process...")
+print(">>> Loading LoRA model: ", lora_model_name)
 # Load PeftModel
 model = PeftModel.from_pretrained(
             model,
@@ -37,9 +42,9 @@ model = PeftModel.from_pretrained(
         )
 model.eval()
 
-print(">>> ")
+print("/// ")
 print(">>> Start the process of merging the base model and the LoRA model.")
-print(">>> ")
+print("/// ")
 
 # Merge: gpt-neox base model + LoRA model
 key_list = [key for key, _ in model.base_model.model.named_modules() if "lora" not in key]
@@ -52,5 +57,7 @@ for key in key_list:
 
 model = model.base_model.model
 
-print(">>> DONE! Saving merge model to: ", output_dir)
+print(">>> Saving merge model to: ", output_dir)
 model.save_pretrained(output_dir)
+print("--- DONE!")
+
